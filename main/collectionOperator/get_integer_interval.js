e894b899769388b570f56be6dfc63e421147492a
WangRan <ranwang1995@outlook.com>
2019-12-27 22:26:55 +0800

    [WangRan] feat-1: finish get_integer_interval.js

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,7 +2,21 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var increment;
+  var result = [];
+  if (number_b === number_a) {
+    result.push(number_a);
+  }
+  else {
+    increment = (number_a < number_b) ? 1 : -1;
+    for (var num = number_a; num !== number_b; num += increment) {
+      result.push(num);
+    }
+    result.push(number_b);
+  }
+  return result;
 }
+
 
 module.exports = get_integer_interval;
 
