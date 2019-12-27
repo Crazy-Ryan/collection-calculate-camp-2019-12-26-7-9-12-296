59ea949e0b07d6b92ee3e24d556f02af2ffbe841
Ryan.Wang <ranwang1995@outlook.com>
2019-12-28 01:05:59 +0800

    [WangRan] feat: finish get_letter_interval_2.js

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -2,6 +2,19 @@
 
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
+  var get_integer_interval = require('../../main/collectionOperator/get_integer_interval.js');
+  var result = [];
+  function convertFromNumToChar(ele) {
+    var number = ele -1;
+    if (number < 26) {
+      result.push(String.fromCharCode(number + 97));
+    }
+    else {
+      result.push(String.fromCharCode(number / 26 + 96) + String.fromCharCode(number % 26 + 97));
+    }
+  }
+  get_integer_interval(number_a, number_b).forEach(convertFromNumToChar);
+  return result;
 }
 
 module.exports = get_letter_interval_2;
