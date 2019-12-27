14c5a10318b58b4680830273dd07d803654d1569
WangRan <ranwang1995@outlook.com>
2019-12-27 22:06:48 +0800

    [WangRan] refactor-1: remove the use of arrow function from ES6

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -2,7 +2,10 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
-  return collection.filter(number => (0 === number % 2));
+  function evenJudge(number){
+    return 0 === number % 2;
+  }
+  return collection.filter(evenJudge);
 }
 
 module.exports = collect_all_even;
