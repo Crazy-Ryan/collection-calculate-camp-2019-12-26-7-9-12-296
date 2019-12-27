2f8485f7de00c2d252d5974688ec696faaf8c372
WangRan <ranwang1995@outlook.com>
2019-12-27 22:48:04 +0800

    [WangRan] feat-5: finish get_intersection.js

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -2,6 +2,13 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  var result=[];
+  collection_b.forEach(element => {
+    if(collection_a.includes(element)){
+      result.push(element);
+    }
+  });
+  return result;
 }
 
 module.exports = get_intersection;
