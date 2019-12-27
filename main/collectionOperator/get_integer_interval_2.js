a1ff75e599b74a00e1206c2e70dc5b62d0e07469
WangRan <ranwang1995@outlook.com>
2019-12-27 22:35:42 +0800

    [WangRan] feat-1: finish get_integer_interval_2.js

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,9 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  var get_integer_interval = require('../../main/collectionOperator/get_integer_interval.js');
+  var collect_all_even = require('../../main/collectionOperator/collect_all_even.js');
+  return collect_all_even(get_integer_interval(number_a,number_b));
 }
 
 module.exports = get_integer_interval_2;
