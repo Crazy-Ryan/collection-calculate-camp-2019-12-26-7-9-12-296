e4c74d384a4d845569dfebc912850ee91578a916
Ryan.Wang <ranwang1995@outlook.com>
2019-12-28 01:09:22 +0800

    [WangRan] feat: finish get_union.js

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -2,6 +2,14 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+    var result = collection_a;
+    function judgeInclude(element) {
+      if (!(collection_a.includes(element))) {
+        result.push(element);
+      }
+    }
+    collection_b.forEach(judgeInclude);
+    return result;
 }
 
 module.exports = get_union;
