a867d1b8f54e48e8fc694b8083be3f9d6b8ad786
WangRan <ranwang1995@outlook.com>
2019-12-28 22:32:23 +0800

    [WangRan] feat: finish compute_average.js

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -2,6 +2,9 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  return collection.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0) / (collection.length);
 }
 
 module.exports = compute_average;
