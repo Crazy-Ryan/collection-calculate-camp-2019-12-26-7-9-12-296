860cd2590dcf3adcd0674f279af7d8facd4c454a
WangRan <ranwang1995@outlook.com>
2019-12-28 20:35:49 +0800

    [WangRan] feat: finish grouping_count.js

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -3,6 +3,17 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  var instanceCount =collection.reduce(countInstances,{}) ;
+  function countInstances(allNames, name) { 
+    if (name in allNames) {
+      allNames[name]++
+    }
+    else {
+      allNames[name] = 1
+    }
+    return allNames;
+  }
+  return instanceCount;
 }
 
 module.exports = grouping_count;
