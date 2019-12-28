1899f5eab890b84feae233b6f225adebb44313df
WangRan <ranwang1995@outlook.com>
2019-12-28 21:53:24 +0800

    [WangRan] feat: finish double_dimensional_array_to_one_1.js

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  return collection.reduce(function(accumulator, currentValue){
+    return accumulator.concat(currentValue);
+  },[]);
 }
 
 module.exports = double_to_one;
