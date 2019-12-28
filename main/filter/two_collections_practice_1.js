43d218e22516953a9d158379778b14456efa3cfd
WangRan <ranwang1995@outlook.com>
2019-12-28 21:16:07 +0800

    [WangRan] feat: finish two_collections_practice_1_spec.js

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -3,6 +3,15 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var result = [];
+  function judgeInclude(element) {
+    if (collection_b.includes(element)) {
+      result.push(element);
+    }
+  }
+  collection_a.forEach(judgeInclude);
+
+  return result;
 }
 
 module.exports = choose_common_elements;
