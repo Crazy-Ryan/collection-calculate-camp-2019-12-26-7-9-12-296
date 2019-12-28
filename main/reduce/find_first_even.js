f0b716147f8817b2ca552adf44aa0238a98ce444
WangRan <ranwang1995@outlook.com>
2019-12-28 23:11:32 +0800

    [WangRan] feat: finish find_first_even.js

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -2,6 +2,8 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  var collect_all_even = require('../../main/collectionOperator/collect_all_even.js');
+  return collect_all_even(collection)[0];
 }
 
 module.exports = find_first_even;
