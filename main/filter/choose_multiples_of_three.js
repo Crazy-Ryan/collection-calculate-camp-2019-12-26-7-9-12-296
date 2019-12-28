8146bbc58dd68c34dc66871abf5f0c5e147c7964
WangRan <ranwang1995@outlook.com>
2019-12-28 20:04:16 +0800

    [WangRan] feat: finish choose_multiples_of_three.js

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -3,6 +3,10 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
+  function multipleOfThreeJudge(number) {
+    return 0 === number % 3;
+  }
+  return collection.filter(multipleOfThreeJudge);
 }
 
 module.exports = choose_multiples_of_three;
