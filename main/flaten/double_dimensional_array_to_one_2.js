b71c2f637b2f8cf030224231692bc6614231267a
WangRan <ranwang1995@outlook.com>
2019-12-28 21:56:05 +0800

    [WangRan] feat: finish double_dimensional_array_to_one_2.js

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var choose_no_repeat_number = require("../../main/filter/choose_no_repeat_number.js");
+  var double_to_one = require("../../main/flaten/double_dimensional_array_to_one_1.js");
+  return choose_no_repeat_number(double_to_one(collection));
 }
 
 module.exports = double_to_one;
