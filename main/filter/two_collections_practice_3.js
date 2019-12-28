ccc356c0c0e90e77722695b08735801ffe65f70e
WangRan <ranwang1995@outlook.com>
2019-12-28 21:40:09 +0800

    [WangRan] feat: finish two_collections_practice_3_spec.js

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -3,6 +3,24 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var result;
+  result = collection_a.reduce(function (accumulator, currentValue) {
+    if (isDivisibleByB(currentValue)) {
+      accumulator.push(currentValue);
+    }
+    return accumulator;
+  }, []);
+
+  function isDivisibleByB(num) {
+    var flag = false;
+    collection_b.forEach(element => {
+      if (0 === num % element) {
+        flag = true;
+      }
+    });
+    return flag;
+  }
+  return result;
 }
 
 module.exports = choose_divisible_integer;
