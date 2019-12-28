6d05b999be671effcd1bbc95a4bc2f9ec01dd8c3
WangRan <ranwang1995@outlook.com>
2019-12-28 23:14:17 +0800

    [WangRan] feat: finish find_last_even.js

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -2,6 +2,8 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var collect_all_even = require('../../main/collectionOperator/collect_all_even.js');
+  return collect_all_even(collection).pop();
 }
 
 module.exports = find_last_even;
