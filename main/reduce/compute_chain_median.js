71127d811458b13bb206c2d6c477e09052d66503
WangRan <ranwang1995@outlook.com>
2019-12-28 22:53:56 +0800

    [WangRan] feat: finish compute_chain_median.js

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -2,6 +2,17 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  var rank_asc = require('../../main/map/rank_desc.js');
+  var charArrayInput = collection.split('->');
+  var numArrayInput = charArrayInput.map(x => +x);
+  var length = numArrayInput.length;
+  var ascCollection = rank_asc(numArrayInput);
+  if (0 === length % 2) {
+    return (ascCollection[length / 2 - 1] + ascCollection[length / 2]) / 2;
+  }
+  else {
+    return ascCollection[(length - 1) / 2];
+  }
 }
 
 module.exports = compute_chain_median;
