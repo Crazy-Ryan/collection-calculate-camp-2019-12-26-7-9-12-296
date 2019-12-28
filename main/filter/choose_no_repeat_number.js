c153badedd04dcdecb7fce31d17d55db29f90912
WangRan <ranwang1995@outlook.com>
2019-12-28 20:08:14 +0800

    [WangRan] feat: finish choose_no_repeat_numbers.js

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -3,6 +3,14 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
+  var output = [];
+  collection.forEach(pickNoRepeat);
+  function pickNoRepeat(element) {
+    if (!output.includes(element)) {
+      output.push(element);
+    }
+  }
+  return output;
 }
 
 module.exports = choose_no_repeat_number;
