847e61e7a97eca019e82d187da43a7398ccebe4c
WangRan <ranwang1995@outlook.com>
2019-12-28 20:01:20 +0800

    [WangRan] feat: finish choose_even.js

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -3,6 +3,10 @@
 function choose_even(collection) {
 
   //在这里写入代码
+  function evenJudge(number) {
+    return 0 === number % 2;
+  }
+  return collection.filter(evenJudge);
 }
 
 module.exports = choose_even;
