b1ebdcf14e03b654b13d649b11b75e6f7d1aea25
WangRan <ranwang1995@outlook.com>
2019-12-28 22:09:13 +0800

    [WangRan] feat: finish number_map_to_word_over_26.js

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,13 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+var number_map_to_word_over_26 = function (collection) {
+  // return ['a','m','aa','ad','y','aa'];
+  function convertFromNumToChar(num) {
+    var number = num - 1;
+    return (number < 26 ? String.fromCharCode(number + 97)
+      : String.fromCharCode(number / 26 + 96) + String.fromCharCode(number % 26 + 97));
+  }
+
+  return collection.map(x => convertFromNumToChar(x));
 };
 
 module.exports = number_map_to_word_over_26;
