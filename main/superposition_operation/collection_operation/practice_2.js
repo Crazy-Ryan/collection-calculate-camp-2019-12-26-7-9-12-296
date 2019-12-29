b1a658b986649eaa65508c68152746ba4863fbef
WangRan <ranwang1995@outlook.com>
2019-12-29 11:13:38 +0800

    [WangRan] feat: finish practice_2.js

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -3,6 +3,13 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var mapToThreeMultiplesAddTwo = function (collection) {
+    return collection.map(x => (x * 3 + 2));
+  };
+  function oddJudge(number) {
+    return 1 === number % 2;
+  }
+  return mapToThreeMultiplesAddTwo(collection.filter(oddJudge));
 }
 
 module.exports = hybrid_operation_to_uneven;
