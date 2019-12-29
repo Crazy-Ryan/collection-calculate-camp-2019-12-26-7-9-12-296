0c7a5455195d75df652402d8fada451552848153
WangRan <ranwang1995@outlook.com>
2019-12-29 20:04:39 +0800

    [WangRan] feat: finish section_2 practice_1.js

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,16 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var result = [];
+  var grouping_count = require("../../../main/filter/grouping_count.js");
+  var instanceCount = grouping_count(collection);
+  var instanceKeys = Object.keys(instanceCount);
+  for (var index = 0; index < instanceKeys.length; index++) {
+    var itemCount = {};
+    itemCount.key = instanceKeys[index];
+    itemCount.count = instanceCount[instanceKeys[index]];
+    result.push(itemCount);
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
