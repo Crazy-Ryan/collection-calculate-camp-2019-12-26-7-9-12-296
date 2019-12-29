52111838eb2787099bc94d87e80da40f01968be5
WangRan <ranwang1995@outlook.com>
2019-12-29 11:01:34 +0800

    [WangRan] feat: finish letter_mapping_2.js

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -3,6 +3,8 @@
 function average_to_letter(collection) {
 
   //在这里写入代码
+  var compute_average = require('../../../main/reduce/compute_average.js');
+  return String.fromCharCode(Math.ceil(compute_average(collection)) + 96);
 }
 
 module.exports = average_to_letter;
