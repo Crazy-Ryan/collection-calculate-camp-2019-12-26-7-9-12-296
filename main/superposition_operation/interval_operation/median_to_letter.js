d6651b4126b13d3a7ce224c07951d88c98f24314
WangRan <ranwang1995@outlook.com>
2019-12-29 11:33:42 +0800

    [WangRan] feat: finish median_to_letter.js

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -3,6 +3,13 @@
 function median_to_letter(collection) {
 
   //在这里写入代码
+  var compute_median = require('../../../main/reduce/compute_median.js');
+  function convertFromNumToChar(num) {
+    var number = num - 1;
+    return (number < 26 ? String.fromCharCode(number + 97)
+      : String.fromCharCode(number / 26 + 96) + String.fromCharCode(number % 26 + 97));
+  }
+  return convertFromNumToChar(Math.ceil(compute_median(collection)));
 }
 
 module.exports = median_to_letter;
