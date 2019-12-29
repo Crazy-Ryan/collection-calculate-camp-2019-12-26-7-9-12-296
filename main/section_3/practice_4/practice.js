95069bdef66fce9742a3eba1dcce155bed3fc682
WangRan <ranwang1995@outlook.com>
2019-12-29 21:38:28 +0800

    [WangRan] feat: finish section_3_practice_4.js

--- main/section_3/practice_4/practice.js
+++ main/section_3/practice_4/practice.js
@@ -1,5 +1,8 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var count_same_elements = require("../../../main/section_2/practice_2/practice.js");
+  var updateArraysByMatch = require("../../../main/section_3/practice_2/practice.js");
+  return updateArraysByMatch(count_same_elements(collection_a), object_b);
 }
 
 module.exports = create_updated_collection;
