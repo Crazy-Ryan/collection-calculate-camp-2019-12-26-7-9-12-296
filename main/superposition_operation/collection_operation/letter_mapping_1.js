8032d5ab3840ed2d040f81ff30260003050abb0e
WangRan <ranwang1995@outlook.com>
2019-12-29 10:45:11 +0800

    [WangRan] feat: finish letter_mapping_1.js

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -3,6 +3,9 @@
 function even_to_letter(collection) {
 
   //在这里写入代码
+  var collect_all_even = require('../../../main/collectionOperator/collect_all_even.js');
+  var number_map_to_word = require('../../../main/map/number_map_to_word.js');
+  return (number_map_to_word(collect_all_even(collection)));
 }
 
 module.exports = even_to_letter;
