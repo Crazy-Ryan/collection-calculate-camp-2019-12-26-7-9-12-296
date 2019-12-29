fc111d01f04fb44ca97163aade79e112ecc1d00c
WangRan <ranwang1995@outlook.com>
2019-12-29 10:36:51 +0800

    [WangRan] feat: finish split_to_zero.js

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,13 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var result = [number];
+  do {
+    number = Math.round(10 * (number - interval)) / 10;
+    result.push(number);
+  }
+  while (number > 0);
+  return result;
 }
 
 module.exports = spilt_to_zero;
