ffad8f77befbc1c96b223654d997f2fcd38fe038
WangRan <ranwang1995@outlook.com>
2019-12-29 19:46:57 +0800

    [WangRan] feat: finish section_1 practice_4.js

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,13 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var result=[];
+  function judgeInclude(element) {
+    if (object_b.value.includes(element.key)) {
+      result.push(element.key);
+    }
+  }
+  collection_a.forEach(judgeInclude);
+  return result;
 }
 
 module.exports = collect_same_elements;
