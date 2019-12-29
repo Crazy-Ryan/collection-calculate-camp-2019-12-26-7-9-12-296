c5ae4c0bfc6e7863f898c657c461bcb933524f5f
WangRan <ranwang1995@outlook.com>
2019-12-29 19:40:25 +0800

    [WangRan] feat: finish section_1 practice_3.js

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var get_intersection = require('../../../main/collectionOperator/get_intersection.js');
+  return get_intersection(object_b.value, collection_a);
 }
 
 module.exports = collect_same_elements;
