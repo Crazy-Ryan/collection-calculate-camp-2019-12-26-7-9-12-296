49642d07d43c894dd0ea36c45c440d28add3830b
WangRan <ranwang1995@outlook.com>
2019-12-29 16:13:59 +0800

    [WangRan] feat: finish is_exist_element.js

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,14 @@
 'use strict';
-var is_exist_element = function(collection,element){
+var is_exist_element = function (collection, element) {
+  var chooseEvenIndex = function (collection) {
+    return collection.reduce(function (accumulator, currentValue, index) {
+      if (0 === index % 2) {
+        accumulator.push(currentValue);
+      }
+      return accumulator;
+    }, []);
+  }
+  return chooseEvenIndex(collection).includes(element);
 
 };
 module.exports = is_exist_element;
