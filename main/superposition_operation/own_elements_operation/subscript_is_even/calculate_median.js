2923f48d63f039f85295e1deebb5f30946c30f8c
WangRan <ranwang1995@outlook.com>
2019-12-29 14:22:23 +0800

    [WangRan] feat: finish calculate_median.js

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,13 @@
 'use strict';
-var calculate_median = function(collection){
+var calculate_median = function (collection) {
+  var compute_median = require('../../../../main/reduce/compute_median.js');
+  var choose_even_index = collection.reduce(function (accumulator, currentValue, index) {
+    if (0 === (index + 1) % 2) {
+      accumulator.push(currentValue);
+    }
+    return accumulator;
+  }, []);
+  return compute_median(choose_even_index);
 
 };
 module.exports = calculate_median;
