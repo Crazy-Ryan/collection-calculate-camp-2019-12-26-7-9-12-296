a4583e6ee5b00db88f153cb911e523a2ffc714fa
WangRan <ranwang1995@outlook.com>
2019-12-29 11:23:07 +0800

    [WangRan] feat: finish amount_even.js

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -3,6 +3,9 @@
 function amount_even(collection) {
 
   //在这里写入代码
+  var choose_even = require("../../../main/filter/choose_even.js");
+  var compute_elements_sum = require('../../../main/reduce/compute_sum.js');
+  return compute_elements_sum(choose_even(collection));
 }
 
 module.exports = amount_even;
