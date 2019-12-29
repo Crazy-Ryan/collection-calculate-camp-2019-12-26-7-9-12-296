f6daf7f2fb4a2891c598cadb4a01dcd7fabaacc5
WangRan <ranwang1995@outlook.com>
2019-12-29 11:16:34 +0800

    [WangRan] feat: finish practice_3.js

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -3,6 +3,14 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var compute_elements_sum = require('../../../main/reduce/compute_sum.js');
+  var mapToThreeMultiplesAddFive = function (collection) {
+    return collection.map(x => (x * 3 + 5));
+  };
+  function oddJudge(number) {
+    return 1 === number % 2;
+  }
+  return compute_elements_sum(mapToThreeMultiplesAddFive(collection.filter(oddJudge)));
 }
 
 module.exports = hybrid_operation_to_uneven;
