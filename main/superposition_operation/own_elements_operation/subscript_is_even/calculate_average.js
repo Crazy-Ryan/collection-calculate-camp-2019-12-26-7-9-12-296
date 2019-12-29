a9fc0fcdbc326b5f5bb221f3aeb33070e8d44e3e
WangRan <ranwang1995@outlook.com>
2019-12-29 11:43:14 +0800

    [WangRan] feat: finish calculate_average.js

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,7 @@
 'use strict';
 var calculate_average = function(collection){
-
+  var compute_average = require('../../../../main/reduce/compute_average.js');
+  var choose_even = require("../../../../main/filter/choose_even.js");
+  return compute_average(choose_even(collection));
 };
 module.exports = calculate_average;
