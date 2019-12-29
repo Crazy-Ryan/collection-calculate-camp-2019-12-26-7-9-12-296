431e259d9983d5f1af0513aba909494711c62177
WangRan <ranwang1995@outlook.com>
2019-12-29 17:14:58 +0800

    [WangRan] feat: finish even_asc_odd_desc.js

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,21 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
-
+var even_asc_odd_desc = function (collection) {
+  var rank_desc = require('../../../main/map/rank_asc.js');
+  var rank_asc = require('../../../main/map/rank_desc.js');
+  function collect_all_even(collection) {
+    function evenJudge(number) {
+      return 0 === number % 2;
+    }
+    return collection.filter(evenJudge);
+  }
+  function collect_all_odd(collection) {
+    function oddJudge(number) {
+      return 1 === number % 2;
+    }
+    return collection.filter(oddJudge);
+  }
+  var evenNumsAsc = rank_asc(collect_all_even(collection));
+  var oddNumsDesc = rank_desc(collect_all_odd(collection));
+  return evenNumsAsc.concat(oddNumsDesc);
 };
 module.exports = even_asc_odd_desc;
