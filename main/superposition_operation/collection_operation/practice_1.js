06142d56fee6cd80424426587d248cf8bd233493
WangRan <ranwang1995@outlook.com>
2019-12-29 11:10:14 +0800

    [WangRan] feat: finish practice_1.js

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -3,6 +3,11 @@
 function hybrid_operation(collection) {
 
   //在这里写入代码
+  var compute_elements_sum = require('../../../main/reduce/compute_sum.js');
+  var mapToThreeMultiplesAddTwo = function (collection) {
+    return collection.map(x => (x * 3 + 2));
+  };
+  return compute_elements_sum(mapToThreeMultiplesAddTwo(collection));
 }
 
 module.exports = hybrid_operation;
