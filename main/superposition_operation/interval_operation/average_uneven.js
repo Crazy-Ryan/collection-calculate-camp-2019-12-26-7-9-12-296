4b4ec1ca4dc4d7c7ebd75497e578b455f6c268df
WangRan <ranwang1995@outlook.com>
2019-12-29 11:28:05 +0800

    [WangRan] feat: finish average_uneven.js

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,11 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  var compute_average = require('../../../main/reduce/compute_average.js');
+  function oddJudge(number) {
+    return 1 === number % 2;
+  }
+  return compute_average(collection.filter(oddJudge));
 }
 
 module.exports = average_uneven;
