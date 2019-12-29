1eab841e080adbc9a576356385c4b52edfa6d388
WangRan <ranwang1995@outlook.com>
2019-12-29 17:03:08 +0800

    [WangRan] feat: finish single_element.js

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,20 @@
 'use strict';
-var single_element = function(collection){
+var single_element = function (collection) {
+  var chooseEvenIndex = function (collection) {
+    return collection.reduce(function (accumulator, currentValue, index) {
+      if (0 === (index + 1) % 2) {
+        accumulator.push(currentValue);
+      }
+      return accumulator;
+    }, []);
+  }
+  function singleNums(accumulator, currentValue, index, array){
+    if (array.indexOf(currentValue) === array.lastIndexOf(currentValue)) {
+      accumulator.push(currentValue);
+    }
+    return accumulator;
+  }
+  return chooseEvenIndex(collection).reduce(singleNums, []);
 
 };
 module.exports = single_element;
