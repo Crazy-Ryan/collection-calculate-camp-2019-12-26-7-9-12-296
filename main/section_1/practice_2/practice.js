240b6f22e36b52f3ffd603de73115c17256765ba
WangRan <ranwang1995@outlook.com>
2019-12-30 14:13:45 +0800

    [WangRan] fix section_1_practice_1.js

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,7 +1,7 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
   var get_intersection = require('../../../main/collectionOperator/get_intersection.js');
-  return get_intersection(collection_b.flat(), collection_a);
+  return get_intersection(collection_b[0], collection_a);
 }
 
 module.exports = collect_same_elements;
