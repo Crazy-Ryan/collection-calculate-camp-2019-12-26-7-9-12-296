f180734c89099e2a9ae8d9a71d27fc81de562cd9
Ryan.Wang <ranwang1995@outlook.com>
2020-01-28 00:27:44 +0800

    [WangRan] refactor: rewrite scripts under map with ES6

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,13 +1,12 @@
 'use strict';
 var number_map_to_word_over_26 = function (collection) {
   // return ['a','m','aa','ad','y','aa'];
-  function convertFromNumToChar(num) {
-    var number = num - 1;
+
+  return collection.map(num => {
+    let number = num - 1;
     return (number < 26 ? String.fromCharCode(number + 97)
       : String.fromCharCode(number / 26 + 96) + String.fromCharCode(number % 26 + 97));
-  }
-
-  return collection.map(x => convertFromNumToChar(x)); //TODO: 建议函数inline
+  });
 };
 
 module.exports = number_map_to_word_over_26;
